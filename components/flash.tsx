43cbc6e6ca7b6b78d821579a949f53f17605751c
Brayden Wilkinson <braydenwilkinson@Braydens-MacBook-Air.local>
2026-06-23 16:52:25 -0400

    remove remaining components

--- components/flash.tsx
+++
@@ -1,4 +0,0 @@
-export function Flash({ error, success }: { error?: string; success?: string }) {
-  if (!error && !success) return null;
-  return <div className={`flash ${error ? "error" : "success"}`}>{error ?? success}</div>;
-}
